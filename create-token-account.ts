ccf2770d772952e6c4e60a1891f7328efb032233
Evansens <ivanpulupshyk21@gmail.com>
2025-04-13 17:05:25 +0300

    new lessons update

---
+++ create-token-account.ts
@@ -0,0 +1,46 @@
+import "dotenv/config";
+import { getExplorerLink } from "@solana-developers/helpers";
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  clusterApiUrl,
+} from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount } from "@solana/spl-token";
+
+let privateKey = process.env.PK;
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+console.log(
+  `🔑 Our pubic key is: ${sender.publicKey.toBase58()}`
+);
+
+const tokenMintAccount = new PublicKey(
+    "7FD5TUSWPes5ufgRc7SWbVVny2bpR9HtVeBApeFWC8Ww"
+  );
+  const recipient = new PublicKey("AS8ZJtcwix1nXwqRbw2oaZ6JxkgX7Tp3eqW6VjN3Yadx");
+  
+  const tokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    sender,
+    tokenMintAccount,
+    recipient
+  );
+  
+  console.log(`Token Account: ${tokenAccount.address.toBase58()}`);
+  
+  const link = getExplorerLink(
+    "address",
+    tokenAccount.address.toBase58(),
+    "devnet"
+  );
+  
+  console.log(`✅ Created token account: ${link}`);
+  
---
+++ mint-tokens.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import { Connection, Keypair, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { mintTo } from "@solana/spl-token";
+import { getExplorerLink } from "@solana-developers/helpers";
+
+let privateKey = process.env.PK;
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+const tokenMintAccount = new PublicKey(
+  "7FD5TUSWPes5ufgRc7SWbVVny2bpR9HtVeBApeFWC8Ww"
+);
+
+const recipientAssociatedTokenAccount = new PublicKey(
+    "EktEa51xqndoaoRZNq4nGLYaRKvQN8VAA6tzggkLd2cy"
+  );
+  
+  const transactionSignature = await mintTo(
+    connection,
+    sender,
+    tokenMintAccount,
+    recipientAssociatedTokenAccount,
+    sender,
+    10 * MINOR_UNITS_PER_MAJOR_UNITS
+  );
+  
+  const link = getExplorerLink("transaction", transactionSignature, "devnet");
+  
+  console.log("✅ Success!");
+  console.log(`Mint Token Transaction: ${link}`);
+  
